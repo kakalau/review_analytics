85444501e59157059ef7d2b91eec5f3dd88e0035
Karen Lau <karenkarenlau@gmail.com>
2019-12-30 23:29:29 +0800

    cal avg length of review

--- read.py
+++ read.py
@@ -7,5 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-print(data[0])
+print('The files are being process. No of data is', len(data))
 
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('Average is', sum_len/len(data))
