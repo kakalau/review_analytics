e8d888dca01d66db43859a428fc1e1ad4a87380c
Karen Lau <karenkarenlau@gmail.com>
2020-01-01 11:29:50 +0800

    add filter

--- read.py
+++ read.py
@@ -14,3 +14,11 @@ for d in data:
 	sum_len = sum_len + len(d)
 
 print('Average is', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('There are',len(new), 'data len is less than 100')
+print(new[0])
+print(new[1])
